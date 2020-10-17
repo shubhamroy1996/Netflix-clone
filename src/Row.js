58ea4cf2edf40f7ff14a5382621c2e19404f094a
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2020-10-17 23:31:46 +0530

    add movie path and css

--- src/Row.js
+++ src/Row.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import axios from './axios';
-import requests from './requests';
 import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
-function Row({ title, fetchUrl }) {
+
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
 
-    useEffect( () => {
+    useEffect(() => {
         //if [], run once when the row loads, and dont run it again
         async function fetchData () {
             const request = await axios.get(fetchUrl);
@@ -17,16 +17,21 @@ function Row({ title, fetchUrl }) {
 
         fetchData();
     }, [fetchUrl]);
+
+    console.table(movies)
+
     return (
-        <div class="row">
+        <div className="row">
             <h2>{title}</h2>
-            <div className="row_posters">
+
+            <div className="row__posters">
             {/* row_posters */}
             {movies.map((movie) => (
                 <img 
                 key={movie.id}
-                className="row_poster"
-                src={`${base_url}${movie.poster_path}`} 
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${base_url}${
+                    isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                     alt={movie.name} />
             ))}
             </div>
