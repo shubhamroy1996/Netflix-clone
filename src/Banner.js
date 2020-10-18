2a7372fc6ffd85e4131869b725d95b09a6d4fda8
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2020-10-18 13:08:49 +0530

    add banneer file and its css

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import requests from './requests';
+import "./Banner.css";
 
 function Banner() {
     const [movie, setMovie] = useState([]);
@@ -18,7 +19,10 @@ function Banner() {
         fetchData();
     }, []);
 
+    function truncate(str, n){
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
 
+    }
     return (
         <header className="banner"
             style={{
@@ -31,19 +35,24 @@ function Banner() {
 
             <div className="banner__contents">
 
-            <h1>
+            <h1 className="banner_title">
                 {movie?.title || movie?.name || movie?.original_name}
             </h1>
             <div className="banner__buttons">
-                <button className="banner__buttons">Play</button>
-                <button className="banner__buttons">My List</button>
+                <button className="banner__button">Play</button>
+                <button className="banner__button">My List</button>
 
             </div>
             {/* background image */}
             {/* title */}
             {/* div> 2 buttons */}
             {/* description */}
+        <h1 className="banner__description">
+            {truncate(movie?.overview, 150)}
+            </h1>
             </div>
+            <div className="banner--fadeBottom" />
+
         </header>
     )
 }
