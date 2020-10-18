fd0c64b530b82dde6b2760fff6a26c02818249bf
shubhamroy1996 <shubham.roy1996.sr@gmail.com>
2020-10-18 21:13:47 +0530

    add nav and logic to play trailer

---
+++ src/Nav.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import "./Nav.css";
+
+
+function Nav() {
+    const [show, handleShow] = useState(false)
+
+useEffect(() => {
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > 100) {
+            handleShow(true);
+
+        } else handleShow(false);
+    });
+
+    return() => {
+        window.removeEventListener("scroll");
+    };
+}, []);
+
+    return (
+        <div className={`nav ${show && "nav__black"}`}>
+            <img
+            className="nav__logo"
+            src="https://pmcvariety.files.wordpress.com/2020/05/netflix-logo.png?w=600"
+            alt="Netflix Logo" />
+            
+            <img 
+            className="nav__avatar"
+            src="https://i.pinimg.com/564x/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.jpg"
+            alt="Netflix Logo" />
+        </div>
+    )
+}
+
+export default Nav
--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import YouTube from 'react-youtube';
 import axios from './axios';
 import './Row.css';
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState("");
 
     useEffect(() => {
         //if [], run once when the row loads, and dont run it again
@@ -19,7 +22,25 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }, [fetchUrl]);
 
     console.table(movies)
-
+    const opts ={
+        height: "390",
+        width: "100%",
+        playerVars: {
+            autoplay: 1,
+        },
+    };
+    const handleClick = (movie) => {
+    if (trailerUrl) {
+    setTrailerUrl("");
+    } else {
+    movieTrailer(movie?.name || "")
+    .then((url) => {
+    const urlParams = new URLSearchParams(new URL(url).search);
+    setTrailerUrl(urlParams.get('v'));
+    })
+    .catch((error) => console.log(error));
+    }
+};
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -29,13 +50,14 @@ function Row({ title, fetchUrl, isLargeRow }) {
             {movies.map((movie) => (
                 <img 
                 key={movie.id}
+                onClick={() =>handleClick(movie)}
                 className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                 src={`${base_url}${
                     isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
                     alt={movie.name} />
             ))}
             </div>
-
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 }
